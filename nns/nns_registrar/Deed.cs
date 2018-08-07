c3f52098b34e3c35e0f549c100172894de5bb728
Jacky <jacky@Jackyde-MacBook-Pro.local>
2018-08-07 19:15:21 +0800

    add newbid function

--- nns/nns_registrar/Deed.cs
+++ nns/nns_registrar/Deed.cs
@@ -1,44 +1,19 @@
-using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
-using Helper = Neo.SmartContract.Framework.Helper;
 using System;
 using System.Numerics;
 
 namespace nns_registrar
 {
-  public class Deed : SmartContract
-  {
-    public byte[] registrar;
+  public class Deed {
     public byte[] owner;
-    public byte[] previousOwner;
-    public uint createOn;
+    public BigInteger createOn;
     public BigInteger value;
-    public Boolean active;
 
-    public Deed(byte[] registrar, byte[] owner, BigInteger value)
+    public Deed(byte[] owner, BigInteger value)
     {
-      this.registrar = registrar;
       this.owner = owner;
       this.value = value;
       this.createOn =  Blockchain.GetHeader(Blockchain.GetHeight()).Timestamp;
-      this.active = true;
-    }
-
-    public void setOwner(byte[] registrar, byte[] newOwner) 
-    {
-      if(this.registrar != registrar)
-      {
-        return;
-      }
-
-      this.previousOwner = this.owner;
-      this.owner = newOwner;
-      Runtime.Notify(new Object[] { "set new owner", newOwner});
-    }
-
-    public void setRegistrar(byte[] newRegistrar)
-    {
-      this.registrar = newRegistrar;
     }
   }
 }
--- nns/nns_registrar/Registrar.cs
+++ nns/nns_registrar/Registrar.cs
@@ -7,54 +7,41 @@ using System.Numerics;
 
 namespace nns_registrar
 {
-  public class Auction {
-    public string domain;                 // include tld
-    public BigInteger registrationDate;   // now + totalAuctionLength
-    public BigInteger value;
-    public BigInteger highestBid;
-  }
 
   public class Registrar : SmartContract
   {
-    const int aDay = 86400;  // second unit
+    const int aDay = 86400;  // seconds
     const int totalAuctionLength = aDay * 5;
     const int revealPeriod = aDay * 2;
-    const int minPrice = 1; // neovm 不支援浮點數, 顆顆
+    const int minPrice = 1; // NeoVM 不支援浮點數
 
-    public byte[] rootNode;
-    public Registrar(byte[] rootNode)
+    //TODO: Registry 部署後需要宣吿 Appcall
+    [Appcall("77e193f1af44a61ed3613e6e3442a0fc809bb4b8")]
+    static extern object registryCall(string method, object[] arr);
+
+    public static readonly byte[] rootNode = SmartContract.Sha256("nns".AsByteArray());
+
+    public Registrar()
     {
-      this.rootNode = rootNode;
     }
 
-    #region domain name to hash
-    //域名转hash算法
-    // e.g. aaa.bb.test =>{"test","bb","aa"}
-    static byte[] nameHash(string domain)
+    public static byte[] nameHash(string label)
     {
-      return SmartContract.Sha256(domain.AsByteArray());
-    }
-    static byte[] nameHashSub(byte[] roothash, string subdomain)
-    {
-      var bs = subdomain.AsByteArray();
+      var bs = label.AsByteArray();
       if (bs.Length == 0) {
-        return roothash;
+        return bs;
       }
-      var domain = SmartContract.Sha256(bs).Concat(roothash);
+      var domain = SmartContract.Sha256(bs).Concat(rootNode);
       return SmartContract.Sha256(domain);
     }
-    static byte[] nameHashArray(string[] domainarray)
+    
+    static byte[] bidkey(string label, byte[] who)
     {
-      byte[] hash = nameHash(domainarray[0]);
-      for (var i = 1; i < domainarray.Length; i++)
-      {
-          hash = nameHashSub(hash, domainarray[i]);
-      }
-      return hash;
+      var bs = label.AsByteArray();
+      var bsWho = SmartContract.Sha256(bs).Concat(who);
+      return SmartContract.Sha256(bsWho);
     }
-    #endregion
 
-    #region mode enum
     public enum DomainUseState
     {
       Open, 
@@ -64,9 +51,7 @@ namespace nns_registrar
       Reveal, 
       NotYetAvailable
     }
-    #endregion
 
-    #region save/get auction data
     private static void saveAuction(Auction auction) {
       byte[] key = nameHash(auction.domain);  // use domain hash as storage's key
       byte[] doublezero = new byte[] { 0, 0 };
@@ -95,8 +80,8 @@ namespace nns_registrar
       Storage.Put(Storage.CurrentContext, key, value);
     }
 
-    private static Auction getAuction(string domain) {
-      byte[] key = nameHash(domain);  // use domain hash as storage's key
+    private static Auction getAuction(string label) {
+      byte[] key = nameHash(label); 
       byte[] data = Storage.Get(Storage.CurrentContext, key);
       Auction auction = new Auction();
       if(data.Length == 0) {
@@ -132,15 +117,70 @@ namespace nns_registrar
 
       return auction;
     }
-    #endregion
+
+    private static void saveDeed(string label, byte[] who, Deed deed) {
+      byte[] key = bidkey(label, who);
+      byte[] doublezero = new byte[] { 0, 0 };
+
+      // deed.owner
+      var data = deed.owner;
+      var datalen = ((BigInteger)data.Length).AsByteArray().Concat(doublezero).Range(0, 2);
+      var value = datalen.Concat(data);
+
+      // deed.createOn
+      data = deed.createOn.AsByteArray();
+      datalen = ((BigInteger)data.Length).AsByteArray().Concat(doublezero).Range(0, 2);
+      value = datalen.Concat(data);
+
+      // deed.value
+      data = deed.value.AsByteArray();
+      datalen = ((BigInteger)data.Length).AsByteArray().Concat(doublezero).Range(0, 2);
+      value = datalen.Concat(data);
+
+      Runtime.Notify(new Object[] { "save deed", label});
+      Storage.Put(Storage.CurrentContext, key, value);
+    }
+
+    private static Deed getDeed(string label, byte[] who) {
+      byte[] key = bidkey(label, who);
+      byte[] data = Storage.Get(Storage.CurrentContext, key);
+
+      Deed deed = new Deed(null, 0);
+      if(data.Length == 0) {
+        return deed;
+      }
+
+      int seek = 0;
+      int len = 0;
+
+      // deed.owner
+      len = (int)data.Range(seek, 2).AsBigInteger();
+      seek += 2;
+      deed.owner = data.Range(seek, len);
+      seek += len;
+
+      // deed.createOn
+      len = (int)data.Range(seek, 2).AsBigInteger();
+      seek += 2;
+      deed.createOn = data.Range(seek, len).AsBigInteger();
+      seek += len;
+
+      // deed.value
+      len = (int)data.Range(seek, 2).AsBigInteger();
+      seek += 2;
+      deed.value = data.Range(seek, len).AsBigInteger();
+      seek += len;
+
+      return deed;
+    }
 
     // get now timestamp
     private static uint now () {
       return Blockchain.GetHeader(Blockchain.GetHeight()).Timestamp;
     }
 
-    private static DomainUseState DomainState(string domain) {
-      Auction auction = getAuction(domain);
+    private static DomainUseState DomainState(string label) {
+      Auction auction = getAuction(label);
       if(auction.domain == null) {
         return DomainUseState.Open;
       }
@@ -165,26 +205,80 @@ namespace nns_registrar
     public static Object Main(string operation, Object[] args)
     {
       var selfHash = ExecutionEngine.ExecutingScriptHash;
-      if(operation == "startAuction") {
-        startAuction((string)args[0]);
+      // byte[] who = ExecutionEngine.GetCallingScriptHash;
+
+      if(operation == "startAuctionAndBid") {
+        return startAuctionAndBid((string)args[0], (byte[])args[1], (BigInteger)args[2]);
+      } else if(operation == "getAuctionState") {
+        return getAuctionState((string)args[0]);
       }
 
-      return true;
+      return false;
     }
 
-    public static Boolean startAuction(string domain) {
-      var state = DomainState(domain);
+    public static Boolean isAvailable(string label) {
+      DomainUseState state = DomainState(label);
       if(state == DomainUseState.Auction) {
         return true;
       }
 
+      if(state == DomainUseState.Owned || state == DomainUseState.Forbidden || 
+         state == DomainUseState.Reveal || state == DomainUseState.NotYetAvailable) {
+        return false;
+      }
+      return true;
+    }
+
+    /**
+     * @ 建立二級域名的拍賣
+     * @ param string label 拍賣的二級域名
+     * @ return boolean 
+     */
+    public static Boolean startAuction(string label) {
+      if(isAvailable(label) == false) {
+        return false;
+      }
+
       Auction newAuction = new Auction();
-      newAuction.domain = domain;
+      newAuction.domain = label;
       newAuction.registrationDate = now() + totalAuctionLength;
       newAuction.value = 0;
       newAuction.highestBid = 0;
       saveAuction(newAuction);
+      Runtime.Notify(new Object[] { "create auction", label });
       return true;
     }
+
+    public static Boolean newBid(string label, byte[] who, BigInteger value) {
+      if(isAvailable(label) == false) {
+        return false;
+      }
+
+      var checkBid = getDeed(label, who);
+      if(checkBid.value != 0) {
+        // already bid
+        return false;
+      }
+      Deed newDeed = new Deed(who, value);
+      saveDeed(label, who, newDeed);
+      Runtime.Notify(new Object[] { "create new bid", label });
+      return true;
+    }
+
+    public static Boolean startAuctionAndBid(string label, byte[] who, BigInteger value) {
+      var result = startAuction(label);
+      if(result == false) {
+        return false;
+      }
+      return newBid(label, who, value);
+    }
+
+    public static DomainUseState getAuctionState(string label) {
+      return DomainState(label);
+    }
+    
+
+
+
   }
 }
